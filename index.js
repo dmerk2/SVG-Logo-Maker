51d3561023743e63cbda5d0c0fa36c7588e406ea
Dan Merkin <dan.merkin@gmail.com>
2023-08-25 22:37:55 -0400

    Made createLogo function and centered the text inside the svg

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const createLogo = require("./lib/shapes")
 
 const questions = [
   {
@@ -11,12 +12,34 @@ const questions = [
     type: "list",
     name: "shape",
     message: "Select the sape you would like for you logo",
-    choices: ["circle", "square", "triangle"],
+    choices: ["Circle", "Triangle", "Square"],
+  },
+  {
+    type: "input",
+    name: "color",
+    message: "What color would you like for your logo?",
   },
 ];
 
-const init = () => {
-  inquirer.prompt(questions);
+// Function to write README file
+const writeToFile = (fileName, data) => {
+  fs.writeFileSync(fileName, data, (err) => console.error("Error: ", err));
 };
 
+// Initializing app
+const init = () => {
+  inquirer
+    .prompt(questions)
+    // Take the responses from the questions and create readme
+    .then((responses) => {
+      const logo = createLogo(responses);
+      writeToFile("./examples/logo.svg", logo);
+      console.log("Generated logo.svg!");
+    })
+    .catch((err) => {
+      console.error("Error: ", err);
+    });
+};
+
+// Function call to initialize app
 init();
