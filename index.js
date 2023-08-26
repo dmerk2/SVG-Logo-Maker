1378dba06fda6817e36b469b3c789da3b1ee376f
Dan Merkin <dan.merkin@gmail.com>
2023-08-25 23:02:45 -0400

    Create validate function to test for 3 characters and correct hexadecimal value

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const createLogo = require("./lib/shapes")
+const createLogo = require("./lib/shapes");
 
 const questions = [
   {
     type: "input",
     name: "text",
-    message: "Enter the text you would like to display in your logo",
+    message: "Enter 3 characters to display in your logo.",
+    validate: function (input) {
+      if (input.length === 3) {
+        return true;
+      } else {
+        return "Please enter 3 characters";
+      }
+    },
   },
   {
     type: "list",
@@ -17,11 +24,38 @@ const questions = [
   {
     type: "input",
     name: "color",
-    message: "What color would you like for your logo?",
+    message: "What color or hexademial value would you like for your logo?",
+    validate: function (input) {
+      if (isValidColor(input)) {
+        return true;
+      } else {
+        return "Please enter a valid color or hexadecimal value.";
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "fontColor",
+    message:
+      "What color or hexademial value would you like for your font to be for your logo?",
+    validate: function (input) {
+      if (isValidColor(input)) {
+        return true;
+      } else {
+        return "Please enter a valid color or hexadecimal value.";
+      }
+    },
   },
 ];
 
-// Function to write README file
+const isValidColor = (input) => {
+  // Check if the input is a valid hexadecimal color code
+  const hexColorRegex = /^#([0-9a-fA-F]{3}){1,2}$/;
+
+  return hexColorRegex.test(input);
+};
+
+// Function to create logo.svg file
 const writeToFile = (fileName, data) => {
   fs.writeFileSync(fileName, data, (err) => console.error("Error: ", err));
 };
@@ -30,7 +64,7 @@ const writeToFile = (fileName, data) => {
 const init = () => {
   inquirer
     .prompt(questions)
-    // Take the responses from the questions and create readme
+    // Take the responses from the questions and create logo.svg
     .then((responses) => {
       const logo = createLogo(responses);
       writeToFile("./examples/logo.svg", logo);
@@ -41,5 +75,4 @@ const init = () => {
     });
 };
 
-// Function call to initialize app
 init();
